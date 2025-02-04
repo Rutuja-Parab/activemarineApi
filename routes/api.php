0c47763ebca407ec039d07034cc5a8de11b13bdc
Rutuja <rutujaparab@transpost.co>
2025-02-04 15:48:53 +0530

    push routes

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,10 @@
 
 use App\Http\Controllers\TallyController;
 use App\Http\Middleware\ApiKeyMiddleware;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hello', function () {
     return response()->json(['message' => 'Hello, API!']);
 });
 
-Route::post('/postAllData', [TallyController::class, 'postAllData'])
-    ->middleware(ApiKeyMiddleware::class);
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\TallyController;
 use App\Http\Middleware\ApiKeyMiddleware;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('/getAllData', [TallyController::class, 'getAllData'])
     ->middleware(ApiKeyMiddleware::class);
+
+    Route::post('/postAllData', [TallyController::class, 'postAllData'])
+    ->middleware(ApiKeyMiddleware::class)->middleware(VerifyCsrfToken::class);
 
